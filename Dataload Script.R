9ff3f9a469b80261b67509c11e6b10470e01a0da
Serpurty <92155478+Serpurty@users.noreply.github.com>
2023-03-07 17:14:45 -0800

    Add files via upload

---
+++ Dataload Script.R
@@ -0,0 +1,9 @@
+library(dplyr)
+
+db <- read.csv('Dataload.csv')
+db$Episode.Start.Date <- gsub("T", " ", db$Episode.Start.Date)
+db$Episode.Start.Date <- as.POSIXct(db$Episode.Start.Date,
+                                    format = "%Y-%m-%d %H:%M:%S")
+db$Date <- as.Date(db$Episode.Start.Date)
+day_frequency <- db %>% group_by(Date) %>% tally()
+tail_frequency <- db %>% group_by(Airplane.Tail, Date < '2022-06-10') %>% tally()
